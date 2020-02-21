3e5251ea1b78e2cc1e7f0bce69f4ccdc1e2d9c8f
Cedric GUSTAVE <cgustave@free.fr>
2020-02-21 23:08:40 +0100

    testrunner: save work in progress

--- testrunner/lxc_agent.py
+++ testrunner/lxc_agent.py
@@ -169,24 +169,93 @@ class Lxc_agent(Agent):
             self._ssh.channel_send(data)
             self._ssh.channel_read()
         else:
-            log.error("Could not recognize send command syntax")
+            log.error("Could not recognize send command syntax in line={}".format(line))
             raise SystemExit
 
-        
     def cmd_check(self, line):
         """
         Processing for command "check"
-        Ex : clt-1:0 check [check_name] "keyword" 
-        Ex : clt-1:0 check [check_name] "keyword" since "server ready"
-        Opens a separated ssh connection conn=0 to search in the tracefile
-        if the keyword could be found.
-        In interactive mode, do not use the same channel as the receiving one
-        for checking, always using :0 for check is a good idea.
+        Ex : clt-1:1 check [check_name] "keyword" 
+        Ex : clt-1:1 check [check_name] "keyword" since "server ready"
+        Process local tracefile looking for a pattern
         Optionaly if 'since "mark"' is added, restrict the search in the
         tracefile after the mark
         Return True if keyword is found otherwise False
         """
         log.info("Enter with line={}".format(line))
+        result = False
+        mark = ""
+        
+        match = re.search("check(\s|\t)+\[(?P<name>.+)\](\s|\t)+\"(?P<pattern>[A-Za-z0-9_\s-]+)\"", line)
+        if match:
+            name = match.group('name')
+            pattern = match.group('pattern')
+            log.debug("name={} pattern={}".format(name, pattern))
+
+            match2 = re.search("\s+since\s+\"(?P<mark>.+)\"",line)
+            if match2:
+                mark = match2.group('mark')
+                log.debug("since mark={}".format(mark))
+            result = self._search_pattern_tracefile(pattern=pattern, mark=mark)
+
+        else :
+            log.error("Could not recognize check command syntax in line={}".format(line))
+            raise SystemExit
+
+        log.debug("Result={}".format(result))
+
+        return result
+
+    def _search_pattern_tracefile(self, pattern="", mark=""):
+        """
+        Search for a pattern in the tracefile
+        Return True if found otherwise False
+        If a mark is provided, first search for the mark, then look from the
+        pattern starting from there.
+        """
+        log.info("Enter with pattern={} mark={}-".format(pattern, mark))
+        result = False
+
+        fname = self.get_trace_filename()
+        log.debug("tracefile={}".format(fname))
+
+        try :
+            fh = open(fname)
+        except:
+            log.error("Tracefile {} can't be opened".format(fname))
+            raise SystemExit
+
+        if mark != "":
+            log.debug("Need to search mark={}".format(mark))
+            flag = True 
+        else:
+            flag = False
+
+        # Need to first look for the mark
+        # ex : ### 200221-19:13:13 server ready ###
+        for line in fh:
+            line = line.strip()
+            print("mark - line={}".format(line))
+            if flag:
+                match = re.search("###\s\d+-\d+:\d+:\d+\s"+mark, line)
+                if match:
+                    log.debug("Found mark={}".format(mark))
+                    flag = False
+            else:
+                match2 = re.search(pattern, line)
+                if match2:
+                    log.debug("Found pattern={}".format(pattern))
+                    result = True
+                    break
+
+            print("pattern - line={}".format(line))
+
+        fh.close()
+        log.debug("result={}".format(result))
+        return result
+
+
+
 
 
 
@@ -197,7 +266,8 @@ class Lxc_agent(Agent):
         """
         log.info("Enter with line={}".format(line))
         
-        self._ssh.close()
+        if self._ssh:
+            self._ssh.close()
 
 
 
